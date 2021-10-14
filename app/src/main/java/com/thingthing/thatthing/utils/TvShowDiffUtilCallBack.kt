ca0da64b12c9d3b59b1bbaa060658b6f5c943719
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-10-14 19:34:39 +0300

    simplify diffutilcallback class

--- app/src/main/java/com/thingthing/thatthing/utils/TvShowDiffUtilCallBack.kt
+++ app/src/main/java/com/thingthing/thatthing/utils/TvShowDiffUtilCallBack.kt
@@ -4,12 +4,10 @@ import androidx.recyclerview.widget.DiffUtil
 import com.thingthing.thatthing.model.TvShow
 
 class TvShowDiffUtilCallBack : DiffUtil.ItemCallback<TvShow>() {
-    override fun areItemsTheSame(oldItem: TvShow, newItem: TvShow): Boolean {
-        return oldItem.id == newItem.id
-    }
+    override fun areItemsTheSame(oldItem: TvShow, newItem: TvShow): Boolean = oldItem.id == newItem.id
 
     override fun areContentsTheSame(oldItem: TvShow, newItem: TvShow): Boolean {
-        return oldItem.id == newItem.id
-                && oldItem.name == newItem.name
+        return oldItem.id == newItem.id &&
+            oldItem.name == newItem.name
     }
 }
