a4b4b6cf355e6d7d94f0f9dd4db31236db32bd67
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-10-15 17:32:22 +0300

    set up viewbinding in details screen

--- app/src/main/java/com/thingthing/thatthing/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/thingthing/thatthing/ui/details/DetailsFragment.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 ThingThing
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.thingthing.thatthing.ui.details
 
 import android.os.Bundle
@@ -5,15 +20,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.thingthing.thatthing.R
+import androidx.fragment.app.viewModels
+import com.thingthing.thatthing.databinding.FragmentDetailsBinding
+import com.thingthing.thatthing.ui.TmdbViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
+    private val viewmodel by viewModels<TmdbViewModel>()
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
